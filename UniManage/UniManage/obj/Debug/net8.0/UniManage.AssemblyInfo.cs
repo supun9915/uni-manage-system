baf5caf651a854c6d3e53c1fc5dfaf0d1628e9da
supun9915 <supunsandaruwan543@gmail.com>
2026-05-01 10:37:10 +0530

    fix my grade page issues

--- UniManage/UniManage/obj/Debug/net8.0/UniManage.AssemblyInfo.cs
+++ UniManage/UniManage/obj/Debug/net8.0/UniManage.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UniManage")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b588d519ae8b4b386f8f16825c3110bb31cb42c9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ef4a0be0b9f9cf1ffaaf522f3c549b34ae76294")]
 [assembly: System.Reflection.AssemblyProductAttribute("UniManage")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UniManage")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
