5ad855ec3c58e74b3599f8ab7be277e81565c484
supun9915 <supunsandaruwan543@gmail.com>
2026-05-02 11:40:40 +0530

    fix password update issues

--- UniManage/UniManage/obj/Debug/net8.0/UniManage.AssemblyInfo.cs
+++ UniManage/UniManage/obj/Debug/net8.0/UniManage.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UniManage")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+111afeceea85ff7b2eec4bb5b7d8f384f2b3e2cb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23189e6a7f21d5644092bbc337afba5113057eea")]
 [assembly: System.Reflection.AssemblyProductAttribute("UniManage")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UniManage")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
